da761fddc1bcc9207681e01c610e5b1ba20592c0
Murad J <agariomadman@gmail.com>
2021-07-07 05:17:10 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http').createServer();
 const io = require('socket.io')(http);
-const port = 3000
+const port = process.env.PORT || 3000
 io.on('connection', (socket) => {
     log('connected')
 })
