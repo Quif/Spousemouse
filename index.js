ebd954d68e1727b1e3e1c873720b03f7db2b2a45
Murad J <agariomadman@gmail.com>
2021-07-07 20:38:01 +0400

    main

--- index.js
+++ index.js
@@ -1,22 +1,114 @@
-const express = require('express');
+console.clear();
+// == SETTINGS ==
+var maxLength = 10; // MAX LENGTH (IN SECONDS)
+var FPS = 100; // FPS OF RECORDING (MAXED AT 100)
 
-var port = process.env.PORT || 3000
-const app = express()
-const server = app.listen(port, () => console.log(`server listening on port: ${port}`))
+// ==
 
-const io = require('socket.io')(server)
-io.on('connection', (socket) => {
-    console.log('New connection!')
-    socket.on('mouseMovement', function(data){
-        socket.broadcast.emit('mouseMovement', data)
-        socket.broadcast.emit('movementAlert')
-        console.log("New mouse movement!")
-    });
-    socket.on('wave', function(){
-        socket.broadcast.emit('wave')
-    })
-    socket.on('disconnected', (evt) => {
-        console.log('disconnected')
-    })
-    socket.broadcast.emit('online')
-})
+var robot = require("robotjs");
+var fs = require('fs');
+var server = "";
+var chalk = require("chalk");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+try {
+  const data = fs.readFileSync(__dirname + '/settings.txt', 'utf8')
+  server = data
+} catch (err) {
+  console.error(err)
+}
+
+var screenSize = robot.getScreenSize();
+
+const io = require("socket.io-client");
+
+awaitConnection()
+
+function awaitConnection(){
+  if(server != ""){
+    start()
+  } else{
+    awaitConnection()
+  }
+}
+
+function start(){
+const socket = io(server);
+
+socket.on("mouseMovement", function (data) {
+  if (data.length > maxLength * FPS) {
+    data = data.slice(0, maxLength * FPS);
+  }
+  read(data);
+});
+
+socket.on("disconnected", function () {
+  socket.emit("disconnect");
+});
+
+socket.on("online", function (data) {
+  console.log(chalk.rgb(255, 0, 106)("Someone else connected!"));
+});
+
+socket.on("movementAlert", function (data) {
+  console.log(chalk.yellowBright("Someone just sent a new mouse movement!"));
+});
+
+socket.on("wave", function (data) {
+  console.log(chalk.magenta.underline("Someone waves!"));
+});
+
+socket.on("connect", () => {
+  console.log(chalk.greenBright("SUCCESSFULLY CONNECTED"));
+  GET_MESSAGE();
+});
+
+function GET_MESSAGE() {
+  var arr = [];
+  rl.question(
+    chalk.cyanBright("Type ") +
+      chalk.cyanBright.underline.italic("mouse") +
+      chalk.cyanBright(" to send a mouse movement or ") +
+      chalk.cyanBright.underline.italic("wave") +
+      chalk.cyanBright(" to say hi to others! \n"),
+    (answer) => {
+      if (answer.toLowerCase().includes("mouse")) {
+        rl.question(chalk.cyan("How many seconds? \n"), (answer) => {
+          var writing = setInterval(function () {
+            var mouse = robot.getMousePos();
+            arr.push([mouse.x, mouse.y]);
+          }, 1000 / FPS);
+          setTimeout(function () {
+            console.log("Sent!");
+            socket.emit("mouseMovement", arr);
+            GET_MESSAGE();
+          }, answer * 1000);
+        });
+      } else if (answer.toLowerCase().includes("wave")) {
+        socket.emit("wave");
+        console.log(chalk.magenta.underline("You wave!"));
+        GET_MESSAGE();
+      } else {
+        GET_MESSAGE();
+      }
+    }
+  );
+}
+function read(dataa) {
+  var long = 0;
+  var reading = setInterval(function () {
+    if (dataa.length > long) {
+      robot.moveMouse(dataa[long][0], dataa[long][1]);
+      long++;
+    } else {
+      clearInterval(reading);
+    }
+  }, 1);
+}
+}
