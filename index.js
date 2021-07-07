1882c961b1a4fde9f27ca9f75e8a6f81d8f5bb23
Murad J <agariomadman@gmail.com>
2021-07-07 05:50:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000
 io.on('connection', (socket) => {
     console.log('connected')
+    io.on('mouseMovement', function(data){
+        io.emit('sentMovement', data)
+        io.emit('movementAlert', 'Someone just sent a new mouse movement!')
+    });
+    io.emit('online', 'Someone else connected!')
 })
 io.on('disconnect', (evt) => {
     console.log('disconnected')
