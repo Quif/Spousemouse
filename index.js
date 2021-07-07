c6f7da420384e115f4744ecd6f6b2b912985d0e3
Murad J <agariomadman@gmail.com>
2021-07-07 07:17:25 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const io = require('socket.io')(server)
 io.on('connection', (socket) => {
     console.log('sht my pants')
     io.emit('online', 'Someone else connected!')
-    setInterval(function (){
-        io.emit("still connected")
-    }, 1000)
 })
 io.on('mouseMovement', function(data){
     console.log("poo")
     io.emit('sentMovement', data)
     io.emit('movementAlert', 'Someone just sent a new mouse movement!')
 });
-io.on('disconnect', (evt) => {
+io.on('disconnected', (evt) => {
     console.log('disconnected')
 })
+setInterval(function (){
+    io.emit("still connected")
+}, 1000)
